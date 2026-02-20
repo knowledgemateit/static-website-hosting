2deee82784b6c484089380c8d435ad3ddf877368
knowledgemateit <103619729+knowledgemateit@users.noreply.github.com>
2026-02-20 08:10:48 +0530

    Update resource.tf

--- Static Webiste Hosting Using Terraform/resource.tf
+++ Static Webiste Hosting Using Terraform/resource.tf
@@ -12,14 +12,14 @@ resource "aws_s3_bucket_public_access_block" "bucket1" {
 }
 
 resource "aws_s3_object" "index" {
-  bucket = "rajutraingingtest"
+  bucket = "aws_s3_bucket.bucket1.id"
   key    = "index.html"
   source = "index.html"
   content_type = "text/html"
 }
 
 resource "aws_s3_object" "error" {
-  bucket = "rajutraingingtest"
+  bucket = "aws_s3_bucket.bucket1.id"
   key    = "error.html"
   source = "error.html"
   content_type = "text/html"
@@ -59,4 +59,5 @@ resource "aws_s3_bucket_policy" "public_read_access" {
 EOF
 
 }
+
 
